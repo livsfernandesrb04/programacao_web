5fad019e9dda6042f4bbfa7f098a263aa3ee93bd
Lívia Fernandes Ribeiro Bezerra <101300580+livsfernandesrb04@users.noreply.github.com>
2026-03-08 16:40:44 -0300

    Add atv1

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,14 @@
+let num1 = 31;
+let num2 = 64;
+
+let resultado = num1 + num2;
+console.log(`Resultado da soma entre ${num1} e ${num2} é ${resultado}`);
+
+resultado = num1 - num2;
+console.log(`Resultado da subtração entre ${num1} e ${num2} é ${resultado}`);
+
+resultado = num1 * num2;
+console.log(`Resultado da multiplicação entre ${num1} e ${num2} é ${resultado}`);
+
+resultado = num1 / num2;
+console.log(`Resultado da divisão entre ${num1} e ${num2} é ${resultado}`);
