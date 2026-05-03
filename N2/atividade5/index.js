b191b8d9200055eb0302d9ccbe255a2b2dfe11bf
Lívia Fernandes Ribeiro Bezerra <101300580+livsfernandesrb04@users.noreply.github.com>
2026-05-02 22:03:32 -0300

    Add changes to index.html file

---
+++ N2/atividade5/index.js
@@ -0,0 +1,42 @@
+const express = require ('express');
+const app = express();
+const PORT = 3000;
+
+//Pagina inicial do formulário
+app.get('/', (req, res) => {
+    res.send()
+});
+
+//Envia os dados do formulário para a rota agendamento
+app.get('/agendamento/:', (req, res))
+
+//Adiciona um novo produto ao estoque
+app.get('/adicionar/:id/:nome/:qtd', (req, res) => {
+    let id = Number(req.params.id);
+    let nome = String(req.params.nome);
+    let quant = Number(req.params.qtd);
+
+    res.send()
+});
+
+//Lista todos os produtos do estoque
+app.get('/listar', (req, res) => {
+    res.send()
+});
+
+//remove um produto do estoque
+app.get('/remover/:id', (req, res) => {
+    let id = Number(req.params.id);
+    res.send()
+});
+
+//Altera a quantidade de um produto do estoque
+app.get('/editar/:id/:qtd', (req, res) => {
+    let id = Number(req.params.id);
+    let quant = Number(req.params.qtd);
+    res.send()
+});
+
+app.listen(PORT, () => {
+    console.log(`Applicativo rodando na porta ${PORT}`)
+})
