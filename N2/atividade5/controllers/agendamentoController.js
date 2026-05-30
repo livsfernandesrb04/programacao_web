69381d22e50f11d69e9aee0d18c87a9795d56520
Lívia Fernandes Ribeiro Bezerra <101300580+livsfernandesrb04@users.noreply.github.com>
2026-05-29 21:45:04 -0300

    Add updates to atv5
    Correct the code according to the classes examples

--- N2/atividade5/controllers/agendamentoController.js
+++ N2/atividade5/controllers/agendamentoController.js
@@ -1,15 +1,19 @@
 const AgendamentoConsulta = require('../models/AgendamentoConsultaModel')
 
-let agendamentos = []
-    
 function getIndex(req, res) {
     let dados_erros = {
         erro_nome:req.query.erro_nome,
         erro_sobrenome: req.query.erro_sobrenome,
         erro_cpf: req.query.erro_cpf,
         erro_data_nascimento: req.query.erro_data_nascimento,
-        erro_telefone: req.query.erro_
+        erro_telefone: req.query.erro_telefone,
+        erro_cep: req.query.erro_cep,
+        erro_endereco: req.query.erro_endereco, 
+        erro_clinica: req.query.erro_clinica,
+        erro_especialidade: req.query.erro_especialidade,
+        erro_observacoes: req.query.erro_observacoes
     }
+
     let dados_form = {
         nome: req.query.nome,
         sobrenome: req.query.sobrenome,
@@ -27,26 +31,72 @@ function getIndex(req, res) {
 }
 
 function getAgendamentos () {
-    AgendamentoConsulta.findAll().then(()=>{
+    AgendamentoConsulta.findAll().then((agendamentos)=>{
         res.render("agendamentos.html", {agendamentos})
-        });
-    
-}
-
-function getAgendamento
-
-
-else{
-    AgendamentoConsulta.create(dados_agendamento).then(()=>{
-    res.render('agendamento.html', {dados_agendamento}); //Pesquisar .catch para verificar cenarios de erro
     });
 }
 
+function postAgendamento(req, res) {
+    let dados_agendamento = req.body;
 
+    let query_erros = ""
+    let existe_erro = false;
+    //Erro nome
+    if(
+        dados_agendamento.nome == null ||
+        dados_agendamento.nome.length == 0 ||
+        dados_agendamento.nome.trim() == ""
+    ){
+        query_erros += "erro_nome=true&";
+        existe_erro = true;
+    }
+    else {
+        query_erros += `nome=${dados_agendamento.nome}&`;
+    }
 
+    //Erro sobrenome
+    if(
+        dados_agendamento.sobrenome == null ||
+        dados_agendamento.sobrenome.length == 0 ||
+        dados_agendamento.sobrenome.trim() == ""
+    ){
+        query_erros += "erro_sobrenome=true&";
+        existe_erro = true;
+    }
+    else {
+        query_erros += `nome=${dados_agendamento.sobrenome}&`;
+    }
+
+    //Erro CPF
+    //Erro Data de nascimento
+    //Erro Telefone
+    //Erro CEP
+    //Erro Endereço
+    //Erro Clínica
+    //Erro Especialidade
+    
+
+    //Erro Data da consulta 
+    // precisa ser depois do dia que a consulta foi agendada
+
+    //Erro Hora da consulta 
+    // precisa ser entre as 8:00 até as 18:00
+
+    //Erro Observações
+
+    if(existe_erro){
+        res.redirect('/?'+query_erros);
+    }
+    else{
+        AgendamentoConsulta.create(dados_agendamento).then(()=>{
+            res.render('agendamento.html', {dados_agendamento}); 
+            //Pesquisar .catch para verificar cenarios de erro
+        });
+    }
+}
 
 module.exports = {
     getIndex,
     getAgendamentos,
-    getAgendamento
+    postAgendamento
 }
--- N2/atividade5/index.js
+++ N2/atividade5/index.js
@@ -1,17 +1,28 @@
 const express = require ('express');
 const mustacheExpress = require('mustache-express');
-const agendamentoController = require('');
+const session = require('express-session');
+const agendamentoRouter = require('./routers/agendamentoRouter');
+const usuarioRouter = require('./routers/usuarioRouter');
 const db = require('./db');
+
 const app = express();
 const PORT = 3000;
 
-
 app.engine('html', mustacheExpress());
 app.set('view engine', 'html');
-app.set('views', __dirname + '/views'));
-app.use(express.urlencoded({extended: true}))
+app.set('views', __dirname + '/views');
+app.use(express.urlencoded({extended: true}));
 
-app.use('/', agendamentoRouter)
+app.use(session({
+    secret: 'secret-token',
+    name: 'sessionId',
+    resave: false,
+    saveUninitialized: false
+}));
+
+
+app.use('/', agendamentoRouter);
+app.use('/', usuarioRouter);
 
 db.sync();
 
